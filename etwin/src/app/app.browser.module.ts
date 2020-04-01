10c8a24f39dba711cf5f78c67e4bd300c4d76c55
Charles Samborski <demurgos@demurgos.net>
2020-04-01 14:45:06 +0200

    Create `app.browser.module`

--- etwin/src/app/app.browser.module.ts
+++ etwin/src/app/app.browser.module.ts
@@ -1,14 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppModule } from "./app.module";
-import { ServerModule } from "@angular/platform-server";
 
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
+    BrowserModule,
   ],
   bootstrap: [AppComponent],
 })
-export class AppServerModule {
+export class AppBrowserModule {
 }
--- etwin/src/app/app.module.ts
+++ etwin/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule.withServerTransition({appId: "etwin"}),
+    CommonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  exports: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
