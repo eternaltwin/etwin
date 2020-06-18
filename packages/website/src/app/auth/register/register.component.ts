ac9e9a75b0878c38b4073015e769221dbef2badd
Charles Samborski <demurgos@demurgos.net>
2020-06-18 18:46:54 +0200

    Fix minor styling issues
    - Remove transition for button padding
    - Remove explicit `border-box` configuration
    - Ensure `select` tags do not have a border

--- packages/website/src/app/auth/register/register.component.ts
+++ packages/website/src/app/auth/register/register.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "etwin-register",
   templateUrl: "./register.component.html",
-  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent {
   constructor() {
