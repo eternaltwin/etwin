64e09b1838b94433ba5befb747c8078e72c614c3
Charles Samborski <demurgos@demurgos.net>
2020-12-18 01:04:55 +0100

    [etwin-pg] Add `check` subdommand

--- packages/etwin-pg/src/main/main.ts
+++ packages/etwin-pg/src/main/main.ts
@@ -2,11 +2,13 @@ import { getLocalConfig } from "@eternal-twin/local-config";
 import { withPgPool } from "@eternal-twin/pg-db";
 import pg from "pg";
 
-import { forceCreateLatest, upgradeLatest } from "../lib/index.js";
+import { forceCreateLatest, getState, upgradeLatest } from "../lib/index.js";
 
 export async function main() {
   const args: readonly string[] = process.argv.slice(2);
   switch (args[0]) {
+    case "check":
+      return runCheck();
     case "create":
       return runCreate();
     case "upgrade":
@@ -17,10 +19,15 @@ export async function main() {
   }
 }
 
-async function runUpgrade() {
+async function runCheck() {
   const config = await getLocalConfig();
   return withPgPool(config.db, async (pool: pg.Pool) => {
-    await upgradeLatest(pool);
+    const state = await getState(pool);
+    if (state === 0) {
+      console.log("Empty database");
+    } else {
+      console.log(`Database state: ${state}`);
+    }
   });
 }
 
@@ -28,6 +35,13 @@ async function runCreate() {
   const config = await getLocalConfig();
   return withPgPool(config.db, async (pool: pg.Pool) => {
     await forceCreateLatest(pool);
+  });
+}
+
+async function runUpgrade() {
+  const config = await getLocalConfig();
+  return withPgPool(config.db, async (pool: pg.Pool) => {
+    await upgradeLatest(pool);
   });
 }
 
