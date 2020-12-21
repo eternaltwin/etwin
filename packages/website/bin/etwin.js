73b6091e7db5185a366e8ac28514323830c0ca62
Charles Samborski <demurgos@demurgos.net>
2020-12-21 23:58:11 +0100

    [all] Release v0.4.0
    Add support for Archive, Docs and Dinoparc

--- packages/website/bin/etwin.js
+++ packages/website/bin/etwin.js
@@ -6,6 +6,9 @@ const WASM_FLAG = "--experimental-wasm-modules";
 
 async function main() {
   if (!process.execArgv.includes(WASM_FLAG)) {
+    if (!process.argv.includes("start")) {
+      console.warn("Deprecated implicit start: Add `etwin start` to start the server (`yarn etwin start`).");
+    }
     foregroundChild(
       process.argv0,
       [...process.execArgv, WASM_FLAG, ...process.argv.slice(1)],
