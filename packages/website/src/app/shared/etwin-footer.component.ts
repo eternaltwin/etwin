52e84abc893a8dd499864d331005571de6fdee5c
Charles Samborski <demurgos@demurgos.net>
2020-12-28 02:37:29 +0100

    [frontend] Fix i18n extraction warnings
    Remove duplicated entries and ensure sourcemaps are properly resolved.

---
+++ packages/website/src/app/shared/etwin-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "etwin-footer",
+  templateUrl: "./etwin-footer.component.html",
+  styleUrls: [],
+})
+export class EtwinFooterComponent {
+}
--- packages/website/src/app/shared/shared.module.ts
+++ packages/website/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 
 import { EtwinBarComponent } from "./etwin-bar.component";
+import { EtwinFooterComponent } from "./etwin-footer.component";
 import { LanguagePickerComponent } from "./language-picker.component";
 import { MainLayoutComponent } from "./main-layout.component";
 import { PaginationComponent } from "./pagination.component";
@@ -11,11 +12,11 @@ import { SmallLayoutComponent } from "./small-layout.component";
 import { UserLinkComponent } from "./user-link.component";
 
 @NgModule({
-  declarations: [EtwinBarComponent, LanguagePickerComponent, MainLayoutComponent, PaginationComponent, SmallLayoutComponent, UserLinkComponent],
+  declarations: [EtwinBarComponent, EtwinFooterComponent, LanguagePickerComponent, MainLayoutComponent, PaginationComponent, SmallLayoutComponent, UserLinkComponent],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule,
+    RouterModule.forChild([]),
   ],
   exports: [MainLayoutComponent, SmallLayoutComponent, RouterModule, PaginationComponent, UserLinkComponent],
 })
